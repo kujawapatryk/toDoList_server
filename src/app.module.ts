9a2f1c545eb441c35ff29dfb4282765c2a296a04
kujawapatryk <homik363@wp.pl>
2023-08-23 15:28:11 +0200

    add nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,15 @@ import { TasksController } from "./tasks/tasks.controller";
 import { TasksModule } from "./tasks/tasks.module";
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configDB } from "./config/config";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
   imports: [
+    ConfigModule.forRoot( {isGlobal: true,}),
     TypeOrmModule.forRoot(configDB),
     TasksModule],
   controllers: [AppController, TasksController],
   providers: [AppService],
 })
+
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
   console.log('http://127.0.0.1:3000/');
+
 }
 bootstrap();
