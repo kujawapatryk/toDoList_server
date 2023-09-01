fdc5a832b44585bb9a9b430f12736db6986fdd3b
kujawapatryk <homik363@wp.pl>
2023-09-01 03:11:00 +0200

    End-to-End (E2E) Testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,5 +21,5 @@ describe('AppController (e2e)', () => {
       .expect(200)
       .expect('Hello World!');
   });
-  
+
 });
--- test/tasks.e2e-spec.ts
+++ test/tasks.e2e-spec.ts
@@ -1,80 +1,137 @@
-// import { Test, TestingModule } from '@nestjs/testing';
-// import { AppModule } from '../src/app.module';
-// import { INestApplication } from '@nestjs/common';
-// import * as request from 'supertest';
-// import { TasksModule } from '../src/tasks/tasks.module';
-//
-// describe('AppController (e2e)', () => {
-//     let app: INestApplication;
-//
-//     beforeEach(async () => {
-//         const moduleFixture: TestingModule = await Test.createTestingModule({
-//             imports: [AppModule],
-//         }).compile();
-//
-//         app = moduleFixture.createNestApplication();
-//         await app.init();
-//     });
-//     afterAll(async () => {
-//         await app.close();
-//     });
-//
-//     it('/tasks (GET)', () => {
-//         return request(app.getHttpServer())
-//             .get('/tasks')
-//             .expect(200)
-//             .expect([]);
-//     });
-//
-//     it('/tasks (POST)', () => {
-//         return request(app.getHttpServer())
-//             .post('/tasks')
-//             .send({ content: 'New task', done: false })
-//             .expect(201)
-//             .expect({
-//                 id: expect.any(Number),
-//                 content: 'New task',
-//                 done: false,
-//             });
-//     });
-//
-//     it('/tasks/:id (DELETE)', async () => {
-//         const response = await request(app.getHttpServer())
-//             .post('/tasks')
-//             .send({ content: 'Delete me', done: false });
-//
-//         const idToDelete = response.body.id;
-//
-//         await request(app.getHttpServer())
-//             .delete(`/tasks/${idToDelete}`)
-//             .expect(200)
-//             .expect({ message: 'taskDeleted', status: 'success' });
-//     });
-//
-//     it('/tasks/:id (DELETE - not found)', () => {
-//         return request(app.getHttpServer())
-//             .delete('/tasks/999')
-//             .expect(404);
-//     });
-//
-//     it('/tasks/:id (PATCH)', async () => {
-//         const response = await request(app.getHttpServer())
-//             .post('/tasks')
-//             .send({ content: 'Update me', done: false });
-//
-//         const idToUpdate = response.body.id;
-//
-//         await request(app.getHttpServer())
-//             .patch(`/tasks/${idToUpdate}`)
-//             .send({ done: true })
-//             .expect(200)
-//             .expect({ message: 'taskConfirmation', status: 'success' });
-//     });
-//
-//     it('/tasks/:id (PATCH - not found)', () => {
-//         return request(app.getHttpServer())
-//             .patch('/tasks/999')
-//             .send({ done: true })
-//             .expect(500);
-//     });
-// });
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as supertest from 'supertest';
+import { AppModule } from '../src/app.module';
+import { TasksModule } from '../src/tasks/tasks.module';
+import { TasksService } from '../src/tasks/tasks.service';
+
+
+export class InMemoryTasksService {
+    public tasks = [];
+
+    findAll() {
+        return this.tasks;
+    }
+
+    create(task) {
+        const id = this.tasks.length + 1;
+        task.id = id;
+        this.tasks.push(task);
+        return task;
+    }
+
+    updateTask(id: number, updatedTask) {
+        const taskIndex = this.tasks.findIndex(task => task.id === id);
+        if (taskIndex === -1) {
+            return null;
+        }
+        this.tasks[taskIndex] = { ...this.tasks[taskIndex], ...updatedTask };
+        return this.tasks[taskIndex];
+    }
+
+    deleteTask(id: number) {
+        const taskIndex = this.tasks.findIndex(task => task.id === id);
+        if (taskIndex === -1) {
+            return null;
+        }
+        const removedTask = this.tasks.splice(taskIndex, 1);
+        return removedTask[0];
+    }
+}
+
+describe('Tasks E2E Tests', () => {
+    let app: INestApplication;
+    let request: supertest.SuperTest<supertest.Test>;
+    let taskId: number;
+
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            imports: [AppModule, TasksModule],
+        })
+            .overrideProvider(TasksService)
+            .useClass(InMemoryTasksService)
+            .compile();
+
+        app = module.createNestApplication();
+        await app.init();
+        request = supertest(app.getHttpServer());
+
+        const newTask = { content: 'Test Task', done: true };
+        const response = await request.post('/tasks').send(newTask);
+        expect(response.status).toBe(201);
+        taskId = response.body.id;
+        expect(response.body).toEqual({id:1, content: 'Test Task', done: true })
+
+    });
+
+    afterEach(async () => {
+        await request.delete(`/tasks/${taskId}`);
+        await app.close();
+    });
+
+    describe('Post /tasks/', () => {
+        it('should update a task', async () => {
+            const newTask = { content: 'Test Task POST', done: true };
+            const response = await request.post('/tasks').send(newTask);
+            expect(response.status).toBe(201);
+
+            expect(response.body).toEqual({id:2, content: 'Test Task POST', done: true });
+        });
+    });
+
+    describe('patch /tasks/:id', () => {
+        it('should update a task', async () => {
+            const updatedContent = 'Updated Task';
+
+            const response = await request
+                .patch(`/tasks/${taskId}`)
+                .send({ done: false });
+
+            expect(response.body.message).toEqual('taskConfirmation');
+        });
+    });
+
+    describe('patch /tasks/:id  incorrect data', () => {
+        it('should update a task', async () => {
+            const updatedContent = 'Updated Task';
+
+            const response = await request
+                .patch(`/tasks/incorrectData`)
+                .send({ done: false });
+
+            expect(response.body.message).toEqual('tryLater');
+        });
+    });
+
+    describe('patch /tasks/:id  done: incorrect data', () => {
+        it('should update a task', async () => {
+            const updatedContent = 'Updated Task';
+
+            const response = await request
+                .patch(`/tasks/${taskId}`)
+                .send({ done: 'incorrectDat' });
+
+            expect(response.body.message).toEqual(["Done must be a boolean value"]);
+        });
+    });
+
+    describe('DELETE /tasks/:id incorrect data', () => {
+        it('should delete a task', async () => {
+            const response = await request
+                .delete(`/tasks/incorrectData`)
+                .expect(200);
+
+            expect(response.body.message).toEqual('tryLater');
+        });
+    });
+
+    describe('DELETE /tasks/:id', () => {
+        it('should delete a task', async () => {
+            const response = await request
+                .delete(`/tasks/${taskId}`)
+                .expect(200);
+
+            expect(response.body.message).toEqual('taskDeleted');
+        });
+    });
+});
